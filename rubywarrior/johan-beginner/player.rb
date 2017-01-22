1fa60e01fe32e0ccb2ecd31abd4b525ee6d5e599
Johan Börjesson <johan@Johans-MacBook-Pro.local>
2017-01-22 11:14:49 +0100

    somewhere in the middle of level three

--- rubywarrior/johan-beginner/player.rb
+++ rubywarrior/johan-beginner/player.rb
@@ -1,4 +1,7 @@
 class Player
+  def initialize
+    @healt_last_turn = 0
+  end
   def play_turn(warrior)
     if should_walk? warrior 
       warrior.walk!
@@ -6,8 +9,14 @@ class Player
       warrior.attack!
     end
   end
-
   def should_walk?(warrior)
     return warrior.feel.empty?
+  end
+  def i_am_healty?(warrior)
+    min_health = 10
+    return warrior.health > min_health
+  end
+  def must_rest?(warrior)
+    return jada
   end
 end
